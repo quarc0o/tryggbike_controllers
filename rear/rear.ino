2b70bfc824cc1ba786409a28d8e2815139a32f40
Emmanuel <emmanuelq150@gmail.com>
2022-02-21 10:17:52 +0100

    Individual lights

--- rear/rear.ino
+++ rear/rear.ino
@@ -4,16 +4,20 @@
 #include "RF24.h"
 #include "SPI.h"
 
-int ReceivedMessage[1] = {000}; // Used to store value received by the NRF24L01
+#define LEFT_INDICATOR 2
+#define RIGHT_INDICATOR 3
+
+
+int ReceivedMessage[1] = {0}; // Used to store value received by the NRF24L01
 
 RF24 radio(9,10); // NRF24L01 used SPI pins + Pin 9 and 10 on the UNO
 
-const uint64_t pipe = 0xE6E6E6E6E6E6; // Needs to be the same for communicating between 2 NRF24L01 
-
+const uint64_t pipe = 0xE6E6E6E6E6E5; // Needs to be the same for communicating between 2 NRF24L01 
 
 void setup(void){
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
+  pinMode(LEFT_INDICATOR, OUTPUT);
+  pinMode(RIGHT_INDICATOR, OUTPUT);
   radio.begin(); // Start the NRF24L01
   radio.openReadingPipe(1,pipe); // Get NRF24L01 ready to receive
   radio.startListening(); // Listen to see if information received
@@ -24,17 +28,23 @@ void loop(void){
   while (radio.available())
   {
   radio.read(ReceivedMessage, 1); // Read information from the NRF24L01
-  if (ReceivedMessage[0] == 111) // Indicates switch is pressed
+  if (ReceivedMessage[0] == 0b10) // Turn left
     {
-      Serial.println("Pin high");
-      digitalWrite(2, HIGH); 
+      digitalWrite(LEFT_INDICATOR, HIGH); 
+      digitalWrite(RIGHT_INDICATOR, LOW); 
+    }
+    else if (ReceivedMessage[0] == 0b01) // Turn right
+    {
+      digitalWrite(RIGHT_INDICATOR, HIGH); 
+      digitalWrite(LEFT_INDICATOR, LOW);
     }
     else
-    
     {
-      Serial.println("Pin low");
-      digitalWrite(2, LOW); 
+      digitalWrite(LEFT_INDICATOR, LOW);
+      digitalWrite(RIGHT_INDICATOR, LOW); 
+      
    }
+   Serial.println(ReceivedMessage[0]);
   delay(10);
   }
 }
