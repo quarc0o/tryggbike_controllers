954c538ccc77ed5855ed3eb777ea22233270f4de
Brage Imset <Braimset@gmail.com>
2022-02-21 10:49:06 +0100

    format and clean rear controller code

--- rear/rear.ino
+++ rear/rear.ino
@@ -1,50 +1,46 @@
 
-//Include needed Libraries at beginning
-#include "nRF24L01.h" // NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
+// Include needed Libraries at beginning
 #include "RF24.h"
 #include "SPI.h"
+#include "nRF24L01.h" // NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
 
 #define LEFT_INDICATOR 2
 #define RIGHT_INDICATOR 3
 
-
 int ReceivedMessage[1] = {0}; // Used to store value received by the NRF24L01
 
-RF24 radio(9,10); // NRF24L01 used SPI pins + Pin 9 and 10 on the UNO
+RF24 radio(9, 10); // NRF24L01 used SPI pins + Pin 9 and 10 on the UNO
 
-const uint64_t pipe = 0xE6E6E6E6E6E5; // Needs to be the same for communicating between 2 NRF24L01 
+ // Identifier 
+const uint64_t pipe = 0xE6E6E6E6E6E5;
 
-void setup(void){
+void setup(void) {
   Serial.begin(9600);
   pinMode(LEFT_INDICATOR, OUTPUT);
   pinMode(RIGHT_INDICATOR, OUTPUT);
-  radio.begin(); // Start the NRF24L01
-  radio.openReadingPipe(1,pipe); // Get NRF24L01 ready to receive
-  radio.startListening(); // Listen to see if information received
+
+  radio.begin();
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();
 }
 
-void loop(void){
-  
-  while (radio.available())
-  {
-  radio.read(ReceivedMessage, 1); // Read information from the NRF24L01
-  if (ReceivedMessage[0] == 0b10) // Turn left
+void loop(void) {
+
+  while (radio.available()) {
+    radio.read(ReceivedMessage, 1);
+    if (ReceivedMessage[0] == 0b10) // Turn on left indicator
     {
-      digitalWrite(LEFT_INDICATOR, HIGH); 
-      digitalWrite(RIGHT_INDICATOR, LOW); 
-    }
-    else if (ReceivedMessage[0] == 0b01) // Turn right
+      digitalWrite(LEFT_INDICATOR, HIGH);
+      digitalWrite(RIGHT_INDICATOR, LOW);
+    } else if (ReceivedMessage[0] == 0b01) // Turn on right indicator
     {
-      digitalWrite(RIGHT_INDICATOR, HIGH); 
+      digitalWrite(RIGHT_INDICATOR, HIGH);
       digitalWrite(LEFT_INDICATOR, LOW);
-    }
-    else
-    {
+    } else {
       digitalWrite(LEFT_INDICATOR, LOW);
-      digitalWrite(RIGHT_INDICATOR, LOW); 
-      
-   }
-   Serial.println(ReceivedMessage[0]);
-  delay(10);
+      digitalWrite(RIGHT_INDICATOR, LOW);
+    }
+    Serial.println(ReceivedMessage[0]);
+    delay(10);
   }
 }
