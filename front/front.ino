5fd2d2e15c94b8452a10fc65f448dc22adc20d61
Brage Imset <Braimset@gmail.com>
2022-02-21 10:49:34 +0100

    implement timing front controller timing logic

--- front/front.ino
+++ front/front.ino
@@ -1,34 +1,71 @@
-#include "nRF24L01.h" //NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
 #include "RF24.h"
 #include "SPI.h"
+#include "nRF24L01.h"
 
+#define LEFT_INDICATOR 2
+#define RIGHT_INDICATOR 3
+#define LEFT_BUTTON 4
+#define RIGHT_BUTTON 5
+int SentMessage[1] = {0b00};
 
-#define SwitchPin 2 // Arcade switch is connected to Pin 8 on NANO
-int SentMessage[1] = {0}; // Used to store value before being sent through the NRF24L01
+RF24 radio(9, 10); // NRF24L01 used SPI pins + Pin 9 and 10 on the NANO
 
-RF24 radio(9,10); // NRF24L01 used SPI pins + Pin 9 and 10 on the NANO
+const uint64_t pipe =
+    0xE6E6E6E6E6E5; // Needs to be the same for communicating between 2 NRF24L01
 
-const uint64_t pipe = 0xE6E6E6E6E6E6; // Needs to be the same for communicating between 2 NRF24L01 
-
-
-void setup(void){
+void setup(void) {
   Serial.begin(9600);
 
-  pinMode(SwitchPin, INPUT_PULLUP); // Define the arcade switch NANO pin as an Input using Internal Pullups
-  digitalWrite(SwitchPin,HIGH); // Set Pin to HIGH at beginning
-  
-  radio.begin(); // Start the NRF24L01
+  pinMode(LEFT_BUTTON, INPUT_PULLUP);
+  pinMode(RIGHT_BUTTON, INPUT_PULLUP);
+  pinMode(LEFT_INDICATOR, OUTPUT);
+  pinMode(RIGHT_INDICATOR, OUTPUT);
+
+  radio.begin();               // Start the NRF24L01
   radio.openWritingPipe(pipe); // Get NRF24L01 ready to transmit
 }
 
-void loop(void){
-  if (digitalRead(SwitchPin) == LOW){ // If Switch is Activated
-  SentMessage[0] = 0;
-  radio.write(SentMessage, 1); // Send value through NRF24L01
-}
-  else {
-  SentMessage[0] = 1;
-  Serial.println("test");
-  radio.write(SentMessage, 1);
+void loop(void) {
+  static unsigned int counter{0};
+  static bool left_turn_indicator_activated{false};
+  static bool right_turn_indicator_activated{false};
+  static bool light_high{false};
+
+  // Delay per iteration
+  static constexpr unsigned int delay_time{10};   // [ms]
+  static constexpr unsigned int cycle_length{60}; // iterations
+  counter = (counter + 1) % cycle_length;
+  delay(delay_time);
+
+  if (digitalRead(LEFT_BUTTON) == HIGH) {
+    if (!left_turn_indicator_activated) {
+      counter = 0;
+    }
+    left_turn_indicator_activated = true;
+    right_turn_indicator_activated = false;
+  } else if (digitalRead(RIGHT_BUTTON) == HIGH) {
+    if (!right_turn_indicator_activated) {
+      counter = 0;
+    }
+    left_turn_indicator_activated = false;
+    right_turn_indicator_activated = true;
+  } else {
+    left_turn_indicator_activated = false;
+    right_turn_indicator_activated = false;
   }
+
+  light_high = counter < (cycle_length / 2);
+
+  if (light_high) {
+    if (left_turn_indicator_activated) {
+      SentMessage[0] = 0b10;
+
+    } else if (right_turn_indicator_activated) {
+      SentMessage[0] = 0b01;
+    }
+  } else {
+    SentMessage[0] = 0b00;
+  }
+
+  radio.write(SentMessage, 1);
 }
